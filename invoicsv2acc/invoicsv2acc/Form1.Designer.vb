35635275d16e22f0956c0fbb7eaf2904a3f5bccc
kidlearns <contact.parasor@gmail.com>
2023-12-12 16:27:11 +0800

    added something but still under working progress

--- invoicsv2acc/invoicsv2acc/Form1.Designer.vb
+++ invoicsv2acc/invoicsv2acc/Form1.Designer.vb
@@ -23,6 +23,8 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        Me.Label1 = New System.Windows.Forms.Label
         Me.SuspendLayout()
         '
         'Button1
@@ -34,11 +36,32 @@ Partial Class Form1
         Me.Button1.Text = "Transfer"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(75, 98)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(123, 23)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.Location = New System.Drawing.Point(72, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(126, 23)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -46,5 +69,7 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
--- invoicsv2acc/invoicsv2acc/Form1.vb
+++ invoicsv2acc/invoicsv2acc/Form1.vb
@@ -7,35 +7,52 @@ Public Class Form1
     Dim cnt As Integer = 0
     Dim vHead As New ClassQto
 
+
     Public Function readByRow()
 
-        Dim rec As ArrayList
+        Dim rec As New ArrayList
 
         Using parser As New TextFieldParser(csvFilePath)
             parser.TextFieldType = FieldType.Delimited
             parser.SetDelimiters(",")
 
             Dim marker As Integer = 0
-            While Not parser.EndOfData
 
-                If marker = 0 Then
-                    marker = 1
-                    Continue While
-                End If
+            While Not parser.EndOfData
 
                 Dim fields As String() = parser.ReadFields()
 
                 If fields IsNot Nothing AndAlso fields.Length > 0 Then
+                    Dim rowList As New ArrayList
 
                     For Each field In fields
 
-                        rec.Add(field)
+                        'rec.Add(field)
+                        If marker = 0 Then
+
+                            marker = 1
+                            Continue For
+
+                        End If
+
                         'MsgBox(field & " ")
+                        If field = Nothing Then
+
+                            rec.Add(Nothing)
+                            Continue While
+
+                        End If
+                        'rowList.Add(Convert.ChangeType(field, GetType(Object)))
+                        'rec.Add(convert2any(field))
+                        rec.Add(field)
 
                     Next
 
+
                 End If
+
             End While
+
         End Using
 
         Return rec
@@ -65,8 +82,6 @@ Public Class Form1
 
             End If
 
-
-
         End Using
 
     End Sub
@@ -75,16 +90,30 @@ Public Class Form1
         Dim accessConnStr As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Pacleb\Desktop\temporary\invoice.accdb;Persist Security Info=False;"
 
         Using accessConn As New OleDbConnection(accessConnStr)
-
+            Dim rec2 As New ArrayList
+            Dim count As Integer
             accessConn.Open()
-
+            rec2 = readByRow()
+            count = rec2.Count
+            Label1.Text = "Transferring data of " + rec2.Count
+            'ProgressBar1.Step 
             For i = 0 To data.WCount
 
-                Dim accessCmd As New OleDbCommand("INSERT INTO invoice(Field1, Field2, Field3) VALUES (@Field1, @Field2, @Field3)", accessConn)
+                'Dim accessCmd As New OleDbCommand("INSERT INTO invoice(Field1, Field2, Field3) VALUES (@Field1, @Field2, @Field3)", accessConn)
+                Dim accessCmd As New OleDbCommand("INSERT INTO invoice( " + data.LString(i) + ") VALUES (@Field)", accessConn)
+                Dim k As Integer = 0
+                'readByRow()
+                For j = 0 To rec2.Count
 
-                accessCmd.Parameters.AddWithValue("@Field1", data.LString(i))
-                accessCmd.Parameters.AddWithValue("@Field2", data.LString(i))
-                accessCmd.Parameters.AddWithValue("@Field3", data.LString(i))
+                    If k = data.WCount Then
+                        Exit For
+                    End If
+
+                    accessCmd.Parameters.AddWithValue("@Field", rec2(j))
+                    Label1.Text = "Transfering data " + j + " of " + count
+                    'accessCmd.Parameters.AddWithValue("@Field" + i, data.LString(i))
+                    'accessCmd.Parameters.AddWithValue("@Field" + i, data.LString(i))
+                Next
 
                 accessCmd.ExecuteNonQuery()
 
@@ -97,11 +126,33 @@ Public Class Form1
         MessageBox.Show("Data Transfer completed.")
 
     End Sub
+    Private Function convert2any(ByVal data As String)
+        Try
+
+            Dim targetTypes As Type() = {GetType(String), GetType(Integer), GetType(Double), GetType(Boolean), GetType(Single)}
+            Dim convertedValue As Object
+
+            For Each targetType As Type In targetTypes
+                convertedValue = Convert.ChangeType(data, targetType)
+                MessageBox.Show("Converted Value ({" & targetType.Name & "}): {" & convertedValue & "}")
+            Next
+
+            Return convertedValue
+        Catch ex As Exception
+
+            MessageBox.Show("Error converting the String to one or more data types.")
+
+        End Try
+    End Function
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
 
-        readByCol()
-        'readByRow()
+        'readByCol()
+        readByRow()
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
